43bd138d0e696befd0709ca857a99465322fc665
codecommand90 <egialbert@yahoo.com>
2026-01-20 05:08:06 +0000

    add a MessageController to send the message to the Consumer

---
+++ src/main/java/com/alvirg/kafka_demo/MessageController.java
@@ -0,0 +1,29 @@
+package com.alvirg.kafka_demo;
+
+import com.alvirg.kafka_demo.producer.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/messages")
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    @PostMapping
+    public ResponseEntity<String> sendMessage(
+            @RequestBody
+            String message
+    ){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Message queued successfully");
+
+    }
+}
